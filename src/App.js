5088f40fc5759f55bc67978441bad0c96bfe1947
abdallah-alabed <abd.alabed97@gmail.com>
2021-09-12 16:48:03 +0300

    map works

--- src/App.js
+++ src/App.js
@@ -12,29 +12,33 @@ class App extends Component {
       lat: "",
       lon: "",
       showData: false,
+      location: "",
     };
   }
   handleLocation = (e) => {
     let display_name = e.target.value;
     this.setState({
       display_name: display_name,
-      showData: false
+      showData: false,
     });
   };
   handleSubmit = (e) => {
-    console.log(`${process.env.REACT_APP_LOCATIONIQ_API_KEY}`);
+    console.log(this.state.location);
     e.preventDefault();
     let select = {
       method: "GET",
-      baseURL: `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.display_name}&format=json`
-          };
+      baseURL: `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.display_name}&format=json`,
+    };
     axios(select).then((res) => {
       let responseData = res.data[0];
       this.setState({
-        display_name: responseData.display_name,
         lon: responseData.lon,
         lat: responseData.lat,
+        display_name: responseData.display_name,
+      })
+      this.setState({
         type: responseData.type,
+        location: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.state.lat},${this.state.lon}&zoom=5`,
         showData: true,
       });
     });
@@ -43,12 +47,19 @@ class App extends Component {
   render() {
     return (
       <div>
-        <LocationForm  handleLocation={this.handleLocation} handleSubmit={this.handleSubmit}/>
+        <LocationForm
+          handleLocation={this.handleLocation}
+          handleSubmit={this.handleSubmit}
+        />
         <br></br>
-        {this.state.showData && <Location display_name={this.state.display_name}
-                    lat={this.state.lat}
-                    lon={this.state.lon}
-          />}
+        {this.state.showData && (
+          <Location
+            display_name={this.state.display_name}
+            location={this.state.location}
+            lat={this.state.lat}
+            lon={this.state.lon}
+          />
+        )}
       </div>
     );
   }
