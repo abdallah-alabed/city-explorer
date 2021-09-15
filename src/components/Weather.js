2cd9739ebf557ca5741830b462c1aa88c0947e69
abdallah-alabed <abd.alabed97@gmail.com>
2021-09-15 16:38:05 +0300

    works fine

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,12 +3,15 @@ import { Card } from "react-bootstrap";
 
 class Weather extends Component {
   render() {
+    console.log(this.props);
     return (
-      <Card style={{ width: "30rem" }}>
+      <Card style={{ width: "40rem" }}>
         <Card.Body>
-          <Card.Title>The Weather for the Upcoming 16 Days {this.props.weatherData}</Card.Title>
+          <Card.Title>The Weather for the Upcoming 16 Days</Card.Title>
           <Card.Text>
-            
+            {this.props.weatherData.map((e) => {
+              return `${e.date}: ${e.description} \n`;
+            })}
           </Card.Text>
         </Card.Body>
       </Card>
